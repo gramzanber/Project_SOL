75ab683142353a59dd34c8fdf5958a752452432a
Sean Kelly <seankelly55@gmail.com>
2017-01-23 20:32:20 -0600

    Added gui class that displays hello world on the screen

--- src/controller/Game.java
+++ src/controller/Game.java
@@ -5,6 +5,8 @@
  */
 package controller;
 
+import view.GUI;
+
 /**
  *
  * @author sean
@@ -15,7 +17,7 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new GUI();
     }
     
 }
