71c75294b00604da35fe5925996601ffea4ec79a
Sean Kelly <seankelly55@gmail.com>
2017-02-16 14:15:04 -0600

    added double buffering to render menu and world functions this should remove the flickering issue

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -24,7 +24,7 @@ public class GamePanel extends JPanel
 
     // Off screen rendering
     private Graphics2D g2;
-    private Image dbImage = null;                                               // Double buffer image
+    private Image dbImage = null;   // Double buffer image
     private Image backgroundImage[];                                       // Double buffer image
     private int backgroundLocation;
     private int mainMenuHover;
@@ -48,11 +48,33 @@ public class GamePanel extends JPanel
     
     public void menuRender()
     {
+        //The traditional notion of double-buffering in Java applications is 
+        //fairly straightforward: create an offscreen image, draw to that image 
+        //using the image's graphics object, then, in one step, call drawImage 
+        //using the target window's graphics object and the offscreen image.
+        //this will reduce flicker because the slower process of rendering each 
+        //object is done off screen
+        
+        //make sure the off screen image is defined
+        if (dbImage == null) {
+            // Creates an off-screen drawable image
+            dbImage = createImage(this.getSize().width, this.getSize().height);
+            if (dbImage == null) {
+                System.out.println("Critical Error: dbImage is null");
+                System.exit(1);
+            } else {
+                //create graphics2d object from off screen image
+                g2 = (Graphics2D) dbImage.getGraphics();
+            }
+        }
+        
+        //draw everything on dbimage
+        
         enterKeyPressed = KeyController.getEnterKeyPressed();
         width = getSize().width;
         height = getSize().height;
         
-        g2 = (Graphics2D)this.getGraphics();
+        //g2 = (Graphics2D)this.getGraphics();
         g2.setColor(Color.RED);
         
         if(Math.abs(this.backgroundLocation) >= 350) { this.backgroundLocation = 0; }
@@ -106,13 +128,40 @@ public class GamePanel extends JPanel
             }
             
         }
+
+        //draw the dbimage to the panel
+        this.getGraphics().drawImage(dbImage, 0, 0, this);
+        
     }
     
     public void worldRender() {
+        
+        //The traditional notion of double-buffering in Java applications is 
+        //fairly straightforward: create an offscreen image, draw to that image 
+        //using the image's graphics object, then, in one step, call drawImage 
+        //using the target window's graphics object and the offscreen image.
+        //this will reduce flicker because the slower process of rendering each 
+        //object is done off screen
+        
+        //make sure the off screen image is defined
+        if (dbImage == null) {
+            // Creates an off-screen drawable image
+            dbImage = createImage(this.getSize().width, this.getSize().height);
+            if (dbImage == null) {
+                System.out.println("Critical Error: dbImage is null");
+                System.exit(1);
+            } else {
+                //create graphics2d object from off screen image
+                g2 = (Graphics2D) dbImage.getGraphics();
+            }
+        }
+        
+        //draw everything on dbimage
+        
         width = getSize().width;
         height = getSize().height;
         
-        g2 = (Graphics2D)this.getGraphics();
+        //g2 = (Graphics2D)this.getGraphics();
         g2.setColor(Color.WHITE);
         
         g2.drawImage(backgroundImage[2], 0, 0,getWidth(), getHeight(), this);
@@ -145,6 +194,9 @@ public class GamePanel extends JPanel
             }
 
         }
+        
+        //draw the dbimage to the panel
+        this.getGraphics().drawImage(dbImage, 0, 0, this);
         
     }
     
@@ -216,6 +268,7 @@ public class GamePanel extends JPanel
         // Highlight back button
         g2.drawRect((width / 2) - 70, (height / 2) + 30, 120, 30);
     }
+    
     
     public void gameRender()
     {
